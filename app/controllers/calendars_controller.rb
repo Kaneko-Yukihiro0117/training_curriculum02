1ed2b169dbbe18c49a74506b4ea86b93499cf9a6
Kaneko-Yukihiro0117 <74887355+Kaneko-Yukihiro0117@users.noreply.github.com>
2020-12-03 20:57:40 +0900

    Issue3完了

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -5,7 +5,7 @@ class CalendarsController < ApplicationController
     getWeek
     @plan = Plan.new
   end
-
+  
   # 予定の保存
   def create
     Plan.create(plan_params)
@@ -24,9 +24,9 @@ class CalendarsController < ApplicationController
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
     @todays_date = Date.today
     # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
-
+    
     @week_days = []
-
+   
     plans = Plan.where(date: @todays_date..@todays_date + 6)
 
     7.times do |x|
